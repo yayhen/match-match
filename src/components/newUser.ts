4f02da0fd21e9c2d06bb6a0063f35397dbef5876
yayhen <mlpoknwow@gmail.com>
2021-07-07 20:30:30 +0300

    fix: validation patterns

--- src/components/newUser.ts
+++ src/components/newUser.ts
@@ -27,6 +27,9 @@ class NewUser {
     let firstNameInput = document.createElement('input');
     firstNameInput.className = 'new-user-modal__first-name'
     firstNameInput.placeholder = 'First name';
+    firstNameInput.pattern = '[A-Za-zА-Яа-яЁё]+$';
+    firstNameInput.minLength = 1;
+    firstNameInput.maxLength = 30;
     firstNameInput.oninput = (e) => {
       const target = e.target as HTMLTextAreaElement;
       if(this.fistNameCheck(target.value)===true) {
@@ -54,6 +57,9 @@ class NewUser {
     let lastNameInput = document.createElement('input');
     lastNameInput.className = 'new-user-modal__last-name'
     lastNameInput.placeholder = 'Last name';
+    lastNameInput.pattern = '[A-Za-zА-Яа-яЁё]+$';
+    lastNameInput.minLength = 1;
+    lastNameInput.maxLength = 30;
     lastNameInput.oninput = (e) => {
       const target = e.target as HTMLTextAreaElement;
       if(this.lastNameCheck(target.value)===true) {
@@ -80,6 +86,7 @@ class NewUser {
     let emailInput = document.createElement('input');
     emailInput.className = 'new-user-modal__email'
     emailInput.placeholder = 'Email';
+    emailInput.type = 'email';
     emailInput.oninput = (e) => {
       const target = e.target as HTMLTextAreaElement;
       if(this.emailCheck(target.value)===true) {
@@ -147,7 +154,7 @@ class NewUser {
   }
 
   fistNameCheck(text: string): boolean {
-    let pattern = new RegExp('^[а-яА-ЯёЁa-zA-Z]+$');
+    let pattern = new RegExp('[A-Za-zА-Яа-яЁё]+$');
     return pattern.test(text);
   }
 
@@ -157,7 +164,8 @@ class NewUser {
   } 
 
   emailCheck(text: string): boolean {
-    let pattern = new RegExp("^(?:[-a-z\d\+\*\/\?!{}`~_%&'=^$#]+(?:\.[-a-z\d\+\*\/\?!{}`~_%&'=^$#]+)*)@(?:[-a-z\d_]+\.){1,60}[a-z]{2,6}$");
+    //let pattern = new RegExp("^(?:[-a-z\d\+\*\/\?!{}`~_%&'=^$#]+(?:\.[-a-z\d\+\*\/\?!{}`~_%&'=^$#]+)*)@(?:[-a-z\d_]+\.){1,60}[a-z]{2,6}$");
+    let pattern = new RegExp("^(([().!#$%&'*+\/=?^_`{|}~\u00BF-\u1FFF\u2C00-\uD7FF\w-]+[,\n#\s$&:\n\t]+){2,}){0,}(?:<){0,1}[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9]))(?:>){0,1}$");
     return pattern.test(text);
   }
 
